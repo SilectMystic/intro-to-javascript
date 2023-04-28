5655a8b513b05e3c1e101c4248cfd3a95b41b56f
SilectMystic <cvasquez@brooklynsteamcenter.org>
2023-04-28 14:57:54 -0400

    Added Code

---
+++ stopwatch.js
@@ -0,0 +1,11 @@
+function Start () {
+    alert("You clicked ths start button")
+}
+
+function Stop () {
+    alert("You clicked this button")
+}
+
+function Restart () {
+    alert("You clicked this button")
+}
